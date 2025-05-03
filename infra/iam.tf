db4bfd5701d5a58df758b1241e2122e6f4fd5caf
warre <warre.pessers@gmail.com>
2025-05-03 09:10:40 +0200

    Fix execution role policy principal

---
+++ infra/iam.tf
@@ -0,0 +1,19 @@
+resource "aws_iam_role" "ecs_execution_role" {
+  name               = "alloy-role"
+  assume_role_policy = data.aws_iam_policy_document.ecs_execution_role_policy.json
+}
+
+data "aws_iam_policy_document" "ecs_execution_role_policy" {
+  statement {
+    actions = [
+      "sts:AssumeRole"
+    ]
+
+    principals {
+      type = "Service"
+      identifiers = [
+        "ecs-tasks.amazonaws.com"
+      ]
+    }
+  }
+}
