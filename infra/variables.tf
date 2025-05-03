a1261661a0281347fe9fc4b9987ff6b2c822a88b
warre <warre.pessers@gmail.com>
2025-05-03 16:09:32 +0200

    Add aws account and region vars with default values

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,3 +1,13 @@
+variable "aws_region" {
+  type = string
+  default = "eu-central-1"
+}
+
+variable "aws_account" {
+  type = string
+  default = "783764587482"
+}
+
 variable "default_vpc_id" {
   type = string
   default = "vpc-074afc85a771ded15"
