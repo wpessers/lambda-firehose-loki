0339f0f33aa72a840e1d91da21226816a853c2db
warre <warre.pessers@gmail.com>
2025-05-12 06:33:40 +0200

    Add openapi schema and fix hello world lambda code

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,8 @@ import pino from "pino";
 
 const logger = pino();
 
-const handler = (event: APIGatewayProxyEvent): APIGatewayProxyResult => {
-  logger.info(event);
+const handler = (_event: APIGatewayProxyEvent): APIGatewayProxyResult => {
+  logger.info('Incoming request received');
   return {
     statusCode: 200,
     body: "Hello World",
