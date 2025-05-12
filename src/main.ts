d423183279a235b93bc0c7f5aeccca655e667149
warre <warre.pessers@gmail.com>
2025-05-12 22:54:09 +0200

    Use esbuild build script and zip lambda deployment package in postbuild

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,10 @@ import pino from "pino";
 
 const logger = pino();
 
-const handler = (_event: APIGatewayProxyEvent): APIGatewayProxyResult => {
-  logger.info('Incoming request received');
+export const handler = (_event: APIGatewayProxyEvent): APIGatewayProxyResult => {
+  logger.info("Incoming request received");
   return {
     statusCode: 200,
     body: "Hello World",
   };
 };
-
-module.exports = { handler };
