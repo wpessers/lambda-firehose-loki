fd276e6a2b1c4428f7b8e01d9a965b1034750156
warre <warre.pessers@gmail.com>
2025-05-20 22:31:59 +0200

    Backup firehose data to S3

--- infra/kinesis.tf
+++ infra/kinesis.tf
@@ -6,8 +6,49 @@ resource "aws_kinesis_firehose_delivery_stream" "logs_stream" {
     url = ""
 
     s3_configuration {
-      role_arn   = ""
-      bucket_arn = ""
+      role_arn           = aws_iam_role.s3_backup.arn
+      bucket_arn         = aws_s3_bucket.firehose_backup.arn
+      buffering_size     = 10
+      buffering_interval = 400
     }
   }
+}
+
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    effect  = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["firehose.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "s3_backup" {
+  name               = "firehose-s3-backup-role"
+  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+}
+
+data "aws_iam_policy_document" "s3_policy" {
+  statement {
+    actions = [
+      "s3:AbortMultipartUpload",
+      "s3:GetBucketLocation",
+      "s3:GetObject",
+      "s3:ListBucket",
+      "s3:ListBucketMultipartUploads",
+      "s3:PutObject"
+    ]
+    effect = "Allow"
+    resources = [
+      aws_s3_bucket.firehose_backup.arn,
+      "${aws_s3_bucket.firehose_backup.arn}/*"
+    ]
+  }
+}
+
+resource "aws_iam_role_policy" "s3_backup" {
+  role   = aws_iam_role.s3_backup.name
+  policy = data.aws_iam_policy_document.s3_policy.json
 }
---
+++ infra/s3.tf
@@ -0,0 +1,10 @@
+resource "aws_s3_bucket" "firehose_backup" {}
+
+resource "aws_s3_bucket_public_access_block" "firehose_backup" {
+  bucket = aws_s3_bucket.firehose_backup_bucket
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
