790c67ada92680bf5d67ac5951455a3a6b2c8d25
warre <warre.pessers@gmail.com>
2025-05-20 23:26:00 +0200

    Fix lambda iam incorrect resource names

--- infra/lambda.tf
+++ infra/lambda.tf
@@ -4,7 +4,7 @@ locals {
 
 resource "aws_lambda_function" "hello_world" {
   function_name = local.lambda_function_name
-  role          = aws_iam_role.lambda_execution_role.arn
+  role          = aws_iam_role.hello_world_lambda.arn
 
   filename         = "../dist/lambdas.zip"
   handler          = "lambdas/helloWorldLambda.handler"
@@ -19,22 +19,13 @@ resource "aws_lambda_function" "hello_world" {
 
   logging_config {
     log_format = "JSON"
-    log_group  = aws_cloudwatch_log_group.log_group.arn
+    log_group  = aws_cloudwatch_log_group.export.name
   }
 }
 
-resource "aws_cloudwatch_log_group" "log_group" {
+resource "aws_cloudwatch_log_group" "export" {
   name            = "/aws/lambda/${local.lambda_function_name}"
   log_group_class = "DELIVERY"
-}
-
-resource "aws_lambda_permission" "apigw_invoke_hello_world_lambda" {
-  statement_id  = "AllowAPIGatewayInvoke"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.hello_world.function_name
-  principal     = "apigateway.amazonaws.com"
-
-  source_arn = "${aws_api_gateway_rest_api.hello_world.execution_arn}/*"
 }
 
 data "aws_iam_policy_document" "logs_assume_role" {
@@ -53,24 +44,21 @@ resource "aws_iam_role" "logs_log_export" {
   assume_role_policy = data.aws_iam_policy_document.logs_assume_role.json
 }
 
-data "aws_iam_policy_document" "lambda_log_export" {
+data "aws_iam_policy_document" "log_export_kinesis" {
   statement {
     actions = [
-      "kinesis:DescribeStreamSummary",
-      "kinesis:ListShards",
-      "kinesis:PutRecord",
-      "kinesis:PutRecords"
+      "firehose:PutRecord",
+      "firehose:PutRecordBatch",
+      "firehose:ListTagsForDeliveryStream"
     ]
-    effect = "Allow"
-    resources = [
-      "${aws_kinesis_firehose_delivery_stream.logs_stream.arn}"
-    ]
+    effect    = "Allow"
+    resources = ["${aws_kinesis_firehose_delivery_stream.logs_stream.arn}"]
   }
 }
 
 resource "aws_iam_role_policy" "lambda_log_export" {
-  policy = data.aws_iam_policy_document.lambda_log_export.json
   role   = aws_iam_role.logs_log_export.name
+  policy = data.aws_iam_policy_document.log_export_kinesis.json
 }
 
 resource "aws_cloudwatch_log_subscription_filter" "lambda_log_export" {
@@ -93,7 +81,7 @@ data "aws_iam_policy_document" "assume_role_lambda" {
   }
 }
 
-resource "aws_iam_role" "lambda_log_export" {
+resource "aws_iam_role" "hello_world_lambda" {
   name               = "${local.lambda_function_name}-role"
   assume_role_policy = data.aws_iam_policy_document.assume_role_lambda.json
 }
@@ -102,9 +90,18 @@ data "aws_iam_policy" "lambda_basic" {
   name = "AWSLambdaBasicExecutionRole"
 }
 
-resource "aws_iam_role_policy_attachments_exclusive" "log_export" {
+resource "aws_iam_role_policy_attachments_exclusive" "hello_world_lambda" {
   policy_arns = [
     data.aws_iam_policy.lambda_basic.arn
   ]
-  role_name = aws_iam_role.lambda_log_export.name
+  role_name = aws_iam_role.hello_world_lambda.name
+}
+
+resource "aws_lambda_permission" "apigw_invoke_hello_world_lambda" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.hello_world.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_api_gateway_rest_api.hello_world.execution_arn}/*"
 }
