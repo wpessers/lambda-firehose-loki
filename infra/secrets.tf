7a2341cceacac7b76055629b1712624b295f4050
warre <warre.pessers@gmail.com>
2025-05-30 20:54:59 +0200

    Rename secrets resources

--- infra/secrets.tf
+++ infra/secrets.tf
@@ -1,7 +1,7 @@
-data "aws_secretsmanager_secret" "grafana_cloud_api_key" {
-  name = "grafana-cloud-api-key"
+data "aws_secretsmanager_secret" "grafana_cloud_loki_instance_id" {
+  name = "grafana-cloud-hosted-logs-id"
 }
 
-data "aws_secretsmanager_secret" "grafana_cloud_hosted_logs_id" {
-  name = "grafana-cloud-hosted-logs-id"
+data "aws_secretsmanager_secret" "grafana_cloud_loki_write_token" {
+  name = "grafana-cloud-api-key"
 }
