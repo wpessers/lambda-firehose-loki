7fcacb5a91b250338f67b7dce54d1f85d56fd7a6
warre <warre.pessers@gmail.com>
2025-05-30 20:50:11 +0200

    Set default value for loki aws logs endpoint variable

--- infra/variables.tf
+++ infra/variables.tf
@@ -4,5 +4,6 @@ variable "aws_region" {
 }
 
 variable "grafana_cloud_loki_endpoint" {
-  type = string
+  type    = string
+  default = "https://aws-logs-prod-012.grafana.net/aws-logs/api/v1/push"
 }
